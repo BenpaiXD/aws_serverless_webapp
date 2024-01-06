ccf7f33ea5513218509f98c6be03259dd30de42c
BenpaiXD <b.bliss@rogers.com>
2024-01-06 17:30:00 +0000

    attempt to fix config

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://qr7hk3jrz1.execute-api.ca-central-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };

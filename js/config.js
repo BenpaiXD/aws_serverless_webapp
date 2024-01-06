4ea877305fba174ede1501d10f4246ccfb8319f6
BenpaiXD <b.bliss@rogers.com>
2024-01-06 17:22:40 +0000

    updated config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'ca-central-1_o1ouexb54', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1nb9sabiopdqjuvq20lieuaiek', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ca-central-1' // e.g. us-east-2
+        userPoolId: 'us-east-2_Ii9rKwECm', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1isaqmjns9mb2r7iklv3lddg0u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://qr7hk3jrz1.execute-api.ca-central-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
